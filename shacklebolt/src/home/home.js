bf82b7c0e8052bd8ec166ed5712cd222638f5427
Randy Pitcher <randypitcherii@gmail.com>
2018-09-27 11:35:19 -0500

    added appbar, aws-sdk

--- shacklebolt/src/home/home.js
+++ shacklebolt/src/home/home.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+import LogoutButton from '../components/logout-button';
+
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.grow = {
+      flexGrow: 1,
+    }
+  }
   render() {
     return (
-      <div className="Home">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to Shacklebolt</h1>
-        </header>
+      <div className='root'>
+        <AppBar position="static" color="primary">
+          <Toolbar>
+            <Typography variant="title" color="inherit" style={this.grow} align='left'>
+              Shacklebolt
+            </Typography>
+            <LogoutButton/>
+          </Toolbar>
+        </AppBar>
       </div>
     );
   }
