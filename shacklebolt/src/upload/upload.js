2573caa832213db70fb98d31c7c671e6c0038666
Randy Pitcher <randypitcherii@gmail.com>
2018-10-05 09:06:12 -0500

    added custom tagging with key-value interface (not raw json)

--- shacklebolt/src/upload/upload.js
+++ shacklebolt/src/upload/upload.js
@@ -2,15 +2,28 @@ import React, { Component } from 'react';
 import {Storage, Auth, API} from 'aws-amplify';
 
 import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
 
 import ENV from '../environment-variables';
 import FileInput from '../components/file-input';
-import TagEditor from '../components/tag-editor';
+import TagEditor from './tag-editor';
 import {getDynamoClient} from '../storage/dynamo-client';
 
-export default class Upload extends Component {
+const styles = {
+    tagEditorContainer: {
+        paddingLeft: '20vw',
+        paddingRight: '20vw',
+    },
+};
+
+class Upload extends Component {
     // init state
-    state = { tags: {}, isDisabled: true };
+    state = {
+        tags: [
+            { key: '', value: '', keyMsg: '', valueMsg: '' },
+        ],
+        isDisabled: true,
+    };
 
     /**
      * saves the tag entry into dynamo
@@ -38,9 +51,28 @@ export default class Upload extends Component {
         });
     }
 
+    /**
+     * submits the new file for upload
+     */
     onSubmit = this.onSubmit.bind(this); // bind this
     onSubmit() {
-        // async handler
+        // validate input
+        let errors = [];
+        this.state.tags.forEach(tag => {
+            if (tag.keyMsg) {
+                errors.push(tag.keyMsg);
+            }
+            if (tag.valueMsg) {
+                errors.push(tag.valueMsg);
+            }
+        });
+
+        if (errors.length !== 0) {
+            alert("Error in tags: \n" + errors.toString());
+            return;
+        }
+
+        // no errors. Async handle the submit.
         (async () => {
             // get current user
             let user = await Auth.currentAuthenticatedUser();
@@ -50,13 +82,13 @@ export default class Upload extends Component {
             const groupName = await API.get('shacklebolt', '/group');
             const filename = file.name;
             const s3_key = groupName + '/' + file.name; // TODO: change this to a UUID at some point
-            let tags = [ // TODO: let the user add tag key/values 
+            let tags = this.state.tags.concat([
                 { key: 'filename', value: filename, },
                 { key: 'filetype', value: file.type, },
                 { key: 'created', value: Date.now().toString(), },
                 { key: 'author', value: user.pool.getClientId() },
-            ];
-
+            ]);
+            
             // store the file in s3
             try {
                 await Storage.put(s3_key, file, {
@@ -81,10 +113,56 @@ export default class Upload extends Component {
         })().catch(err => console.log(err));
     }
 
+    /**
+     * update state with new file
+     * 
+     * @param {event} e - the on change event
+     */
     onFileChange = this.onFileChange.bind(this); // bind this
     onFileChange(e) {
         const file = e.target.files[0];
-        this.setState({currentFile: file, isDisabled: false});
+        this.setState({ currentFile: file, isDisabled: false });
+    }
+
+    /**
+     * when any tag value changes, this is called to update state
+     * 
+     * @param {Number} index - index in the tags array to place the update
+     * @param {key, value, keyMsg, valueMsg} tag - updated tag
+     */
+    onTagChange = this.onTagChange.bind(this);
+    onTagChange(index, tag) {
+        this.setState((prev_state, prev_props) => {
+            let tags = prev_state.tags;
+
+            if (tag.key !== undefined) {
+                tags[index].key = tag.key
+            }
+            if (tag.value !== undefined) {
+                tags[index].value = tag.value
+            }
+            if (tag.keyMsg !== undefined) {
+                tags[index].keyMsg = tag.keyMsg
+            }
+            if (tag.valueMsg !== undefined) {
+                tags[index].valueMsg = tag.valueMsg
+            }
+
+            return { tags: tags};
+        });
+    }
+
+    /**
+     * adds a new tag entry into the tags array in state
+     */
+    addTag = this.addTag.bind(this);
+    addTag(){
+        this.setState((prevState, prevProps) => {
+            let tags = prevState.tags;
+            let newTag = { key: '', value: '', keyMsg: '', valueMsg: '' };
+            tags.push(newTag);
+            return {tags: tags}
+        });
     }
 
     render() {
@@ -93,10 +171,26 @@ export default class Upload extends Component {
                 <header className="App-header">
                     <h1 className="App-title">Upload</h1>
                 </header>
-                <FileInput onChange={this.onFileChange}/>
+                <FileInput 
+                    onChange={this.onFileChange}
+                />
                 <br/>
-                <TagEditor/>
-                <br />
+                <div className={this.props.classes.tagEditorContainer}>
+                    <TagEditor
+                        onChange={this.onTagChange}
+                        tags={this.state.tags}
+                    />
+                    <br />
+                    <Button
+                        type='button'
+                        color="primary"
+                        variant='raised'
+                        onClick={this.addTag}>
+                        Add Tag
+                    </Button>
+                </div>
+                
+                <br/>
                 <br/>
                 <Button 
                     type='button' 
@@ -105,8 +199,10 @@ export default class Upload extends Component {
                     disabled={this.state.isDisabled}
                     onClick={this.onSubmit}>
                     Submit
-                </Button> 
+                </Button>
             </form>
         );
     }
 }
+
+export default withStyles(styles)(Upload);
